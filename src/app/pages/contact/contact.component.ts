2db9c6d57a1f55dd2d92f5ad8f611b36b435782f
Carlos Cano <carlcan.93@gmail.com>
2018-12-26 13:00:50 +0100

    fix some responsive problems and disabled contact form

--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Volunteer } from 'src/app/model/volunteer.model';
 import { ContactService } from '../../service/volunteer.service';
 
@@ -6,10 +6,15 @@ import { ContactService } from '../../service/volunteer.service';
   selector: 'app-contact',
   templateUrl: './contact.component.html'
 })
-export class ContactComponent {
+export class ContactComponent implements OnInit {
   volunteer = new Volunteer();
 
   constructor(private contactService: ContactService) {}
+
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+  }
 
   sendContact() {
     console.log(this.volunteer);
