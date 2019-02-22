b74e55db62e7271ddb2965464b36cc3ffbd1af55
carloscano93 <carlcan.93@gmail.com>
2019-02-22 13:01:01 +0100

    add volunteer projects submenu and change view project

--- src/app/component/navbar/navbar.component.ts
+++ src/app/component/navbar/navbar.component.ts
@@ -1,17 +1,24 @@
-import { Component } from '@angular/core';
-import { TranslateService } from '@ngx-translate/core';
+import { Component } from "@angular/core";
+import { TranslateService } from "@ngx-translate/core";
 
 @Component({
-  selector: 'app-navbar',
-  templateUrl: './navbar.component.html'
+  selector: "app-navbar",
+  templateUrl: "./navbar.component.html"
 })
 export class NavbarComponent {
-  lang = localStorage.getItem('lang').toUpperCase();
-  constructor(private translate: TranslateService) {}
+  lang = localStorage.getItem("lang").toUpperCase();
+
+  projects;
+
+  constructor(private translate: TranslateService) {
+    this.translate.get("volunteering.projects").subscribe(key => {
+      this.projects = key;
+    });
+  }
 
   changeLanguage(lang: string) {
     this.translate.use(lang);
     this.lang = lang.toUpperCase();
-    localStorage.setItem('lang', lang);
+    localStorage.setItem("lang", lang);
   }
 }
--- src/app/component/projects/projects.component.ts
+++ src/app/component/projects/projects.component.ts
@@ -1,9 +1,10 @@
-import { Component, Input } from '@angular/core';
-import { Project } from 'src/app/model/project.model';
+import { Component, Input } from "@angular/core";
+import { Project } from "src/app/model/project.model";
 
 @Component({
-  selector: 'app-projects',
-  templateUrl: './projects.component.html'
+  selector: "app-projects",
+  templateUrl: "./projects.component.html",
+  styleUrls: ["./projects.component.css"]
 })
 export class ProjectsComponent {
   @Input()
